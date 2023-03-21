491a033c26dad9a1b506374470dc73bbcda24c6d
Michael Shimeles <michaelshimeles099@gmail.com>
2023-03-21 13:13:01 +0300

    split for readablity purpouse

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:guide/result.dart';
 import './question.dart';
 import './button.dart';
 import './quiz.dart';
-class MyApp extends StatefulWidget{
+import './result.dart';
+
+class MyApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState(){
+  State<StatefulWidget> createState() {
     return MyAppState();
   }
 }
-  class MyAppState extends State<MyApp> {var index = 0;
-  void action(){
-    setState(() {
-       index = index + 1;
-    });
-   
-    print("button pressed " );
-  }
-  
-  Widget build(BuildContext context){
-    var question =[ {'Question':"what/' is ur favorite color",'ans':["black","blue","red","white"]} ,{'Question':"what is ur favorite food " ,"ans":["apple" ,"banana","orange" ,"none" ] }];
-  if (index <question.length){
-  return MaterialApp(
-    home: Scaffold( 
-      appBar:AppBar( title: Text("my first quiz app"),
-      ),
-    body: quiz(action,question,index )
-  ),
-  );}
-  else {
-    return MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("end"),
-    ),
-    body: Text('done'),
-  ),
-);
 
+class MyAppState extends State<MyApp> {
+  var index = 0;
+  void action() {
+    setState(() {
+      index = index + 1;
+    });
+
+    print("button pressed ");
+  }
+
+  Widget build(BuildContext context) {
+    var question = [
+      {
+        'Question': "what/' is ur favorite color",
+        'ans': ["black", "blue", "red", "white"]
+      },
+      {
+        'Question': "what is ur favorite food ",
+        "ans": ["apple", "banana", "orange", "none"]
+      }
+    ];
+    if (index < question.length) {
+      return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("my first quiz app"),
+            ),
+            body: quiz(action, question, index)),
+      );
+    } else {
+      return result();
+    }
   }
 }
-}
-void main(){
+
+void main() {
   runApp(MyApp());
 }
---
+++ lib/result.dart
@@ -0,0 +1,16 @@
+import "package:flutter/material.dart";
+
+class result extends StatelessWidget {
+  result();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Score"),
+        ),
+        body:
+        Center(child:Text('done')),
+      ),
+    );
+  }
+}
